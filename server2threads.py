51ae7888747379c3bd1f9c8871939205e7b26ec2
Dzemoro <dellorpl@gmail.com>
2019-11-23 16:05:17 +0100

    #Seba poprawki

--- server2threads.py
+++ server2threads.py
@@ -106,6 +106,17 @@ def klient1(ip, port):
                 conn.send(Header.Header(0, 6, id, 0).getHeader())
                 # 6 = nie zgadl
 
+        if (int(op) == 6):
+            print("Klient 1 zgaduje: ", -num)
+            if (int(wyliczona) == int(-num)):
+                print("Klient 1 zgadl")
+                conn.send(Header.Header(0, 5, id, 0).getHeader())
+                # 5 = zgadl
+            else:
+                print("Nie zgadnieto")
+                conn.send(Header.Header(0, 6, id, 0).getHeader())
+                # 6 = nie zgadl
+
 
 def klient2(ip, port):
     global wyliczona
